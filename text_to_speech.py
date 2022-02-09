088478e58e126bf0b43f4fe93495ff1f3f618868
DANGERDEVIL07 <66584496+DANGERDEVIL07@users.noreply.github.com>
2022-02-09 22:20:53 +0530

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,9 +1,22 @@
 import pyttsx3
 
 text_speech= pyttsx3.init()
-
+voices = text_speech.getProperty("voices")
 text = input("Please enter the text: ")
+option = input('Press 0 for male voice and 1 for female voice : ')
 
-text_speech.say(text)
-text_speech.runAndWait()
+# for male voice
+if option=="0":
+    text_speech.setProperty("voice", voices[0].id)
+    text_speech.say(text)
+    text_speech.runAndWait()
+
+# for female voice
+elif option=="1":
+    text_speech.setProperty("voice", voices[1].id)
+    text_speech.say(text)
+    text_speech.runAndWait()
+
+else:
+    print(" Wrong Input")
 
